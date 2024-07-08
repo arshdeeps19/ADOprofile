ee6b03c2718076eb0fee3c16d6328e883569e393
arshdeeps19 <99909533+arshdeeps19@users.noreply.github.com>
2024-07-08 14:05:41 -0700

    add project files

---
+++ NewWebApplication/Controllers/UsersController.cs
@@ -0,0 +1,77 @@
+﻿using NewWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewWebApplication.Controllers
+{
+    public class UsersController : Controller
+    {
+        private string connectionString = "Data Source=np:\\\\.\\pipe\\LOCALDB#9E207986\\tsql\\query;Initial Catalog=master;Integrated Security=True";
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User user, HttpPostedFileBase imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the uploaded image file
+                if (imageFile != null && imageFile.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(imageFile.FileName);
+                    string filePath = Path.Combine(Server.MapPath("~/Images"), fileName);
+                    imageFile.SaveAs(filePath);
+                    user.ImagePath = "/Images/" + fileName;
+                }
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Users (UserName, Email, Password, ImagePath) VALUES (@UserName, @Email, @Password, @ImagePath)", con);
+                    cmd.Parameters.AddWithValue("@UserName", user.UserName);
+                    cmd.Parameters.AddWithValue("@Email", user.Email);
+                    cmd.Parameters.AddWithValue("@Password", user.Password);
+                    cmd.Parameters.AddWithValue("@ImagePath", user.ImagePath);
+
+                    cmd.ExecuteNonQuery();
+                }
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            List<User> users = new List<User>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id, UserName, Email, ImagePath FROM Users", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    User user = new User
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        UserName = reader["UserName"].ToString(),
+                        Email = reader["Email"].ToString(),
+                        ImagePath = reader["ImagePath"].ToString(),
+                    };
+                    users.Add(user);
+                }
+            }
+            return View(users);
+        }
+    }
+}
---
+++ NewWebApplication/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewWebApplication.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "Password must match")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string ImagePath { get; set; } // Property to store the image path
+    }
+}
